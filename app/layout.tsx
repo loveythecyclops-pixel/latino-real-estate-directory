48908f60a831a8d629c150d47b01d81a9f306f26
loveythecyclops-pixel <loveythecyclops@gmail.com>
2026-02-19 15:44:10 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { LanguageProvider, useLanguage } from './LanguageContext';
+const { isEnglish } = useLanguage();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
